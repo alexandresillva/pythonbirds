06eda66a59086f3eee76fc129be77d2baa1d634c
alexandresillva <alexandresillva@gmail.com>
2020-06-22 21:48:11 -0300

    Criados atributos de instância nome é idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
-class ExemploPessoa:
-    pass
+class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprientar(self):
+        return f'Olá {self.nome}'
+
+
+if __name__ == '__main__':
+    p = Pessoa("Alexandre Ribeiro")
+    print(p.nome)
+    print(p.idade)
+    print(p.cumprientar())
+
